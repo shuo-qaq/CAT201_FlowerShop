fc243300cde06d19888d933354ff5842f2c2f583
shuo-qaq <239308216+shuo-qaq@users.noreply.github.com>
2025-12-19 01:14:15 +0800

    Create Flower class with properties and constructor

---
+++ src/main/java/Flower.java
@@ -0,0 +1,9 @@
+public class Flower {
+    private String id;
+    private String name;
+    private double price;
+    public Flower(String id, String name, double price) {
+        this.id = id; this.name = name; this.price = price;
+    }
+    public String getName() { return name; }
+}
